05c2145a720bc5ad0756a66c615268206a362c29
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-07-16 19:38:16 +0530

    basic layout

--- workspace/apps/webapp/pages/dashboard.jsx
+++ workspace/apps/webapp/pages/dashboard.jsx
@@ -1,9 +1,9 @@
-import { Paper, Box, IconButton, Typography, Card, useMediaQuery, CardHeader } from "@mui/material";
+import { Paper, Box, Typography, Card, Chip, Tooltip, Button, Divider } from "@mui/material";
 import { BasketCard, Navigation } from "@basketo/web-ui"
-import { useState } from "react";
 import Navbar from "../Components/Navbar";
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Overview from "../Components/dashboard/overview";
+import { Info, InfoRounded } from "@mui/icons-material";
+
 
 const data = [ { pv: 2400 }, { pv: 1398 }, { pv: 9800 }, { pv: 3908 }, { pv: 4800 }, { pv: 3800 }, { pv: 4300 } ];
 
@@ -11,42 +11,31 @@ const Dashboard = () => {
 
   return (
     <Paper variant="window" sx={{display:'flex',justifyContent:'center'}} >
-      {/* <Navbar/> */}
+      <Navbar/>
       <Navigation />
-          
-        <Box sx={{padding:'30px 20px',width:'100%',maxWidth:'lg',
+         {/* 2:1 grid layout  */}
+        <Box sx={{padding:'90px 20px',width:'100%',maxWidth:'lg',
         display:'grid',gridTemplateColumns:{xs:'1fr',md:'2fr 1fr', gap:'20px'}}} >
           
+        {/* left side part */}
         <Box>
-        <Box  >
-          <Typography variant='subtitle2' fontSize='12px' >OVERVIEW</Typography>
-          <Box sx={{display:'grid',marginTop:'20px', gridTemplateColumns:{xs:'1fr',sm:'1fr 1fr 1fr'},gap:'20px'}} >
-
-            {
-              ['NET WORTH', 'TOTAL HOLDINGS', "PROFIT/LOSS"].map((text, i)=>
-              (
-            <Card variant="outlined" key={i} sx={{ padding:'20px', borderRadius:'12px', height:'max-content'}}>
-            {/* <ResponsiveContainer width="100%" height="60%">
-              <LineChart width='100%' height='200px' data={data}>
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} dot={false} />
-              </LineChart>
-            </ResponsiveContainer> */}
-              <Typography variant="h2" >
-                0.00
-                <Typography component='span' variant="caption" >USDT</Typography>
-              </Typography>
-              <Typography variant="caption" fontSize={'12px'} >{text}</Typography>
-            </Card>
-              ))
-            }
+          <Box sx={{margin:'15px 0px', display:'flex',justifyContent:'flex-end'}} >
+            <Chip variant="outlined" sx={{margin:'0px 10px'}} label={'0x124...45345'} />
+            <Chip variant="standard" label={'Ethereum Mainnet'}  />
           </Box>
-        </Box>  
+          
+        <Overview/>  
 
         <Box sx={{marginTop:'20px'}} >
-            <Typography variant='subtitle2' fontSize='12px' >YOUR PORTFOLIO</Typography>
-            <Box sx={{display:'grid',gridTemplateColumns:'1fr 1fr', gap:'20px', marginTop:'20px'}} >
+        <Typography variant='subtitle2' fontSize='12px' sx={{display:'flex',alignItems:'center'}} >
+            YOUR PORTFOLIO 
+            <Tooltip title="List of Baskets you have invested in." >
+                <InfoRounded sx={{height:'0.7em',width:'0.7em', color:'divider'}} /> 
+            </Tooltip>
+        </Typography>
+            <Box sx={{display:'grid',gridTemplateColumns:{xs:'1fr',sm:'1fr 1fr'}, gap:'20px', marginTop:'20px'}} >
             {Array.from({length:10}).map((_,i)=>(
-                <BasketCard 
+                <BasketCard key={i}
                 data={{title:"Defi Basket", 
                       symbol:"DEFB", 
                       growth:{percent:'+0.01', period:'week'}, 
@@ -63,8 +52,16 @@ const Dashboard = () => {
         </Box>
         </Box> 
         <Box>
-          <Card variant="outlined" sx={{width:'100%',height:'500px', borderRadius:'15px'}} >
-
+          <Card variant="outlined" sx={{width:'100%',height:'500px', borderRadius:'15px', padding:'15px 20px'}} >
+            <Box>
+            <Typography variant="h3" >
+            000.00
+              <Typography component='span' variant="caption" >USDT</Typography>
+            </Typography>
+            <Typography variant="caption" fontSize={'12px'} >WALLET BALANCE</Typography>
+            <Button variant='outlined' fullWidth sx={{mt:'20px'}} >Connect different wallet</Button>
+            </Box>
+            <Divider sx={{margin:'20px 0px'}} />
           </Card>
         </Box>
         </Box>
